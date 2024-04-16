b4a2496397b7d1cb8cc70f32aee5a92b791082a2
Paul Corcuera <paulcorcuera.g@gmail.com>
2024-04-16 00:49:31 -0700

    Update

--- src/ECON627_UBC.jl
+++ src/ECON627_UBC.jl
@@ -15,14 +15,15 @@ export ols, Ω, GMM, TSGMM, TSLS, nls, NLGMM, MLE
 function ols(X,Y)
     n = length(Y)
 
-    b = (X'*X)\(X'*Y)
-    res = Y - X*b
+    θhat = (X'*X)\(X'*Y)
+    res = Y - X*θhat
     Xres = X.*res
-    avar = (X'*X)\(Xres'*Xres)/(X'*X)
 
-    se = sqrt.(diag(avar)/n)
+    avar = n*(X'*X)\(Xres'*Xres)/(X'*X)
 
-    return (b = b , se = se)
+    se = sqrt.(diag(avar))
+
+    return (θhat = θhat , se = se)
 end
 
 # Avar for GMM 
@@ -37,14 +38,14 @@ end
 # Linear GMM 
 function GMM(W, Y, X, Z)
     n = length(Y)
-    b=(X'*Z*W*Z'*X)\(X'*Z*W*Z'*Y)
+    θhat=(X'*Z*W*Z'*X)\(X'*Z*W*Z'*Y)
     Γ = Z'*X/n
-    omega = Ω(Y-X*b,Z)
+    omega = Ω(Y-X*θhat,Z)
 
     avar = (Γ'*W*Γ)\(Γ'*W*omega*W*Γ)/(Γ'*W*Γ)
     se = sqrt.(diag(avar)/n)
     
-    return (b = b , se = se)
+    return (θhat = θhat , se = se)
 end
 
 
@@ -61,14 +62,14 @@ function TSGMM(Y,X,Z)
     
     # Two-step efficient GMM
     WGMM=inv(Ω1);
-    b=(X'*Z*WGMM*Z'*X)\(X'*Z*WGMM*Z'*Y)
-    Ω2=Ω(Y-b*X,Z)
+    θhat=(X'*Z*WGMM*Z'*X)\(X'*Z*WGMM*Z'*Y)
+    Ω2=Ω(Y-θhat*X,Z)
     WGMM=inv(Ω2)
 
     avar = inv(Q'*WGMM*Q)/n
     se=sqrt.(avar)
     
-    return  (b = b , se = se)
+    return  (θhat = θhat , se = se)
     
 end
 
@@ -77,13 +78,13 @@ function TSLS(Y, X, Z)
     n = length(Y)
     W = inv(Z'*Z)
 
-    b =(X'*Z*W*Z'*X)\(X'*Z*W*Z'*Y)
+    θhat =(X'*Z*W*Z'*X)\(X'*Z*W*Z'*Y)
     Γ = Z'*X/n
     
     avar = (Γ'*W*Γ)\(Γ'*W*omega*W*Γ)/(Γ'*W*Γ)
     se = sqrt.(diag(avar)/n)
     
-    return (b = b , se = se)
+    return (θhat = θhat , se = se)
 end
 
 ########### Non-linear Models ##################### 
@@ -106,14 +107,14 @@ function nls(f, y, x )
         error("Minimization failed.")
     end
 
-    bhat = Optim.minimizer(o)
+    θhat = Optim.minimizer(o)
 
 
     # Get residuals
-    r_hat = y - f(x, bhat)
+    r_hat = y - f(x, θhat)
 
-    # Get asyvar, we compute the gradient of f with respect to b
-    v = map(i -> ForwardDiff.gradient(z -> f(x[i, :]', z), bhat), 1:n)
+    # Get asyvar, we compute the gradient of f with respect to θhat
+    v = map(i -> ForwardDiff.gradient(z -> f(x[i, :]', z), θhat), 1:n)
     md = vcat(v'...)
 
     me = md .* r_hat; mmd = md' * md
@@ -121,7 +122,7 @@ function nls(f, y, x )
 
     se = sqrt.(diag(avar));
 
-    return (b = bhat, se = se )
+    return (θhat = θhat, se = se )
 end
 
 # Criterion Function for NonLinear GMM is some function Q(θ,Y,X,Z,W)
@@ -133,13 +134,13 @@ function NLGMM(Y,X,Z,Q)
     n = length(Y)
     #first step GMM
     res=optimize(θ->Q(θ,Y,X,Z,W),[0.0],NewtonTrustRegion(); autodiff = :forward)
-    b=Optim.minimizer(res)
+    θhat=Optim.minimizer(res)
     
     #Standard Error
-    avar=ForwardDiff.hessian(θ->Q(θ,Y,X,Z,W),b)
+    avar=ForwardDiff.hessian(θ->Q(θ,Y,X,Z,W),θhat)
     se=sqrt.( diag(inv(avar))/n)
     
-    return (b = b, se = se )
+    return (θhat = θhat, se = se )
 end
 
 
@@ -148,27 +149,27 @@ function MLE(Y,X,LogL)
     #LogL is a function of the data, this is a FUNCTION
 
     res=optimize(b->LogL(b,Y,X),[0.0;0.0;],LBFGS();autodiff=:forward)
-    bhat=Optim.minimizer(res)
+    θhat=Optim.minimizer(res)
 
 
-    avar = ForwardDiff.hessian(b -> LogL(b, Y, X), bhat )
+    avar = ForwardDiff.hessian(b -> LogL(b, Y, X), θhat )
     se =  sqrt.(diag(inv(avar)))
 
-    return (b = bhat, se = se )
+    return (θhat = θhat, se = se )
 end
 
 # Minimum Distance 
 #function MD(π, Y, X, Z,g,W)
-    # π is the dependent variable you want to fit 
+    # π is the dependent variaθhatle you want to fit 
     # g is a function of the data, this is a FUNCTION
 #    Q = (π-g(Y,X,Z))'*W*(π-g(Y,X,Z))
 
     #Minimizer
 #    res=optimize(θ->Q(θ,Y,X,Z,W),[0.0],NewtonTrustRegion(); autodiff = :forward)
-#    bhat=Optim.minimizer(res)
+#    θhathat=Optim.minimizer(res)
     
-    # Get asyvar, we compute the gradient of f with respect to b
-#    v = map(i -> ForwardDiff.gradient(θ->Q(θ,Y,X,Z,W), bhat), 1:n)
+    # Get asyvar, we compute the gradient of f with respect to θhat
+#    v = map(i -> ForwardDiff.gradient(θ->Q(θ,Y,X,Z,W), θhathat), 1:n)
 #    md = vcat(v'...)
 
 #    me = md .* r_hat; mmd = md' * md
@@ -177,10 +178,10 @@ end
 #    se = sqrt.(diag(avar));
 
     #Standard Error
-#    avar=ForwardDiff.hessian(θ->Q(θ,Y,X,Z,W),b)
+#    avar=ForwardDiff.hessian(θ->Q(θ,Y,X,Z,W),θhat)
 #    se=sqrt.( diag(inv(avar))/n)
 
-#    return (b = bhat, se = se )
+#    return (θhat = θhathat, se = se )
 #end
 
 end # module
